92d3adc13f909f6ec16ae776ad606110fbf56934
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-03-14 11:46:41 -0500

    [ADD] estados imperativos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -4,7 +4,9 @@ const SECURITY_CODE = 'valp';
 const initialState = {
     value: '',
     error: false, 
-    loading: false
+    loading: false,
+    deleted: false,
+    confirmed: false
 };
 
 export const UseState = ({ name }) => {
@@ -19,7 +21,8 @@ export const UseState = ({ name }) => {
                     setState({ 
                         ...state, 
                         error: false,
-                        loading: false
+                        loading: false,
+                        confirmed: true,
                     })
                 } else {
                     setState({ 
@@ -32,19 +35,47 @@ export const UseState = ({ name }) => {
         }
     }, [state.loading]);
 
-    return (
-        <div>
-            <h2>Eliminar {name}</h2>
-            <p>Por favor, escribe el código de seguridad.</p>
-
-            {(state.error && !state.loading) && <p>Error: El código es incorrecto</p>}
-            {state.loading && <p>Cargando...</p>}
-
-            <input 
-                value={state.value}
-                placeholer="Código de seguridad"
-                onChange={(event) => setState({ ...state, value: event.target.value })} />
-            <button onClick={() => setState({ ...state, loading: true })}>Comprobar</button>
-        </div>        
-    )
+    if (!state.deleted && !state.confirmed) {
+        return (
+            <div>
+                <h2>Eliminar {name}</h2>
+                <p>Por favor, escribe el código de seguridad.</p>
+    
+                {(state.error && !state.loading) && <p>Error: El código es incorrecto</p>}
+                {state.loading && <p>Cargando...</p>}
+    
+                <input 
+                    value={state.value}
+                    placeholer="Código de seguridad"
+                    onChange={(event) => setState({ ...state, value: event.target.value })} />
+                <button onClick={() => setState({ ...state, loading: true })}>Comprobar</button>
+            </div>        
+        )        
+    } else if (state.confirmed && !state.deleted) {
+        return (
+            <>
+                <p>Pedimos confirmación, ¿Estás seguro?</p>
+                <button onClick={() => setState({ ...state, deleted: true })}>
+                    Si, eliminar
+                </button>
+                <button onClick={() => setState({ ...state, confirmed: false, value: '' })}>
+                    No, me arrepentí
+                </button>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <p>Eliminado con éxito</p>
+                <button onClick={() => setState({ 
+                    ...state, 
+                    deleted: false, 
+                    confirmed: false,
+                    value: '' 
+                })}>
+                    Recuperar
+                </button>
+            </>
+        )
+    }
 } 
